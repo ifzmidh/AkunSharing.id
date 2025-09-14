c4a689c0bbe3812bb8e7998c88a9c40bc17ceade
Hanif Azmi Hafidh <67179398+ifzmidh@users.noreply.github.com>
2025-09-14 12:56:13 +0700

    FIX: Unused Import Library

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "../views/home/Home.vue";
 import LoginRegister from "../views/loginRegister/LoginRegister.vue";
 import Order from "../views/order/Order.vue";
-import Animejs from "../views/home/Animejs.vue";
 
 const routes = [
   { path: "/", component: Home },
